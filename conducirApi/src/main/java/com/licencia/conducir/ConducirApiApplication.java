699df472ac1353ff43b3df3a1190006cbd68f3a6
Jorge <jlramirez1210@gmail.com>
2025-05-08 22:59:41 -0500

    inicio proyecto

---
+++ conducirApi/src/main/java/com/licencia/conducir/ConducirApiApplication.java
@@ -0,0 +1,13 @@
+package com.licencia.conducir;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConducirApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConducirApiApplication.class, args);
+	}
+
+}
---
+++ conducirApi/src/test/java/com/licencia/conducir/ConducirApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.licencia.conducir;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConducirApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
